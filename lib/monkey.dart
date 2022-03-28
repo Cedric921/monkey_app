85e648f05f1208e7527616a8cbe687550942ed49
cedric karungu <ckarungu921@gmail.com>
2022-03-28 16:29:43 +0200

    icon widget to splash screen

--- lib/monkey.dart
+++ lib/monkey.dart
@@ -6,8 +6,13 @@ class Monkey extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: SplashScreen(),
+    return  MaterialApp(
+      theme: ThemeData(
+        primaryColor: const Color(0xFF008AF0),
+        scaffoldBackgroundColor: const Color(0xFFF5F5F5)
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const SplashScreen(),
     );
   }
 }
--- lib/screens/start/splash_scrren.dart
+++ lib/screens/start/splash_scrren.dart
@@ -10,6 +10,35 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: );
+    return SafeArea(
+        child: Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              icon(),
+            ],
+          ),
+        ),
+      ),
+    ));
   }
+}
+
+Widget icon() {
+  return Container(
+    margin : const EdgeInsets.only(bottom: 15),
+    height: 100,
+    width: 100,
+    decoration: const BoxDecoration(
+      shape: BoxShape.circle,
+      image: DecorationImage(
+        image: AssetImage(
+          'assets/images/monkey.jpg',
+        )
+      )
+    ),
+    
+    
+  );
 }
