6401e7e09a126e1a86aee46d5c471d8fddab7cae
cedric karungu <ckarungu921@gmail.com>
2022-03-30 16:13:15 +0200

    add login design

---
+++ lib/screens/auth/login.dart
@@ -0,0 +1,60 @@
+
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  double longSpace = 30;
+  double  shortSpace = 10;
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.only(),
+            child: Column(children: [
+              title(),
+              subtitle("Beneficiez de nos services"),
+              SizedBox(height: longSpace,)
+            ]),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget title() {
+    return const Padding(
+      padding: EdgeInsets.symmetric(vertical: 15),
+      child: Text(
+        "Login",
+        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+
+  Widget subtitle(String subtitle) {
+    return  Padding(
+      padding:  EdgeInsets.only(bottom: 15),
+      child: Text(
+        subtitle,
+        style: const TextStyle(
+          color: Colors.black54
+        ),
+      ),
+    );
+  }
+
+  Widget textField({
+    String 
+  }){
+    return
+  }
+}
